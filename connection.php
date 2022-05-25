826412242d00ec8bd8257d04f3a72e54fc06701f
fred <310bel@gmail.com>
2022-05-25 03:42:57 +0300

    доб задачи Фуников

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = new mysqli('localhost', 'root', '', 'kinomonster');
+$mysqli = new mysqli('localhost', 'root', 'root', 'kinomonster');
 $mysqli->set_charset('utf8_general_ci');
 if(mysqli_connect_errno()) {
     prinf("Соеденение не установлено", mysqli_connect_error());
